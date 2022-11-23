725ba549d477672b31ef53d0494a28bffd333ab5
Chris Gibson <chrischarlesgibson@gmail.com>
2022-11-22 20:01:11 -0600

    thought controller and routes added

---
+++ controllers/thoughtController.js
@@ -0,0 +1,38 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  //get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Get a thought by id
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // create thought and put its id in the users thoughts arrray field
+  updateThought(req, res) {
+    Course.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : Thought.updateMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() => res.json({ message: "thought updated!" }))
+      .catch((err) => res.status(500).json(err));
+  },
+};
